8023bcda6bb930ab1138aaf5bcd67da7920eaf5d
jacekk <jacek.kapias@amsterdamstandard.com>
2021-11-28 02:10:53 +0100

    used typed alias for thunks;

--- index.tsx
+++ index.tsx
@@ -1,4 +1,12 @@
-import { generic, createStore, Generic, Thunk, Action, action, thunk } from 'easy-peasy';
+import {
+    generic,
+    createStore,
+    Generic,
+    Thunk,
+    Action,
+    action,
+    thunk,
+} from "easy-peasy";
 
 interface AuthState {
     foo: Generic<string>;
@@ -8,35 +16,38 @@ interface AuthActions {
     setFoo: Action<AuthModel, string>;
 }
 
-// type ModelThunk<T = void> = Thunk<AuthModel, T, unknown, StoreModel>;
+type ModelThunk = Thunk<AuthModel, string, unknown, StoreModel>;
 
 interface AuthThunks {
-    setFooAsync: Thunk<AuthModel, string, unknown, StoreModel>;
+    setFooAsync: ModelThunk;
+    setBarAsync: Thunk<AuthModel, number, unknown, StoreModel>;
 }
 
 interface AuthModel extends AuthState, AuthActions, AuthThunks {}
 
 const auth: AuthModel = {
-    foo: generic('111'),
+    foo: generic("111"),
     setFoo: action((state, newVal) => {
         state.foo = newVal;
     }),
     setFooAsync: thunk(async (actions, newVal) => {
         actions.setFoo(newVal);
     }),
-}
+    setBarAsync: thunk(async (actions, newVal) => {
+        actions.setFoo(String(newVal).split("").reverse().join(""));
+    }),
+};
 
 interface StoreModel {
-    auth: AuthModel,
+    auth: AuthModel;
 }
 
 const store = createStore({ auth });
 
-
-(async () => {
-    console.log('uno', store.getState());
-    store.getActions().auth.setFoo('222');
-    console.log('dos', store.getState());
-    store.getActions().auth.setFooAsync('333');
-    console.log('tres', store.getState());
-})()
+console.log("uno", store.getState());
+store.getActions().auth.setFoo("222");
+console.log("dos", store.getState());
+store.getActions().auth.setFooAsync("333");
+console.log("tres", store.getState());
+store.getActions().auth.setBarAsync(123);
+console.log("quatro", store.getState());
